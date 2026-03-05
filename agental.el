b9b6491e275463d6522fe760a944484413bae52f
lizqwer scott <lizqwerscott@163.com>
2026-03-05 17:53:33 +0800

    refactor(agental): fix preset-args handling in subagent loop
    - Pass nil as the second argument to `agental-make-preset` to maintain the
    expected function signature
    - Ensure preset-args are properly forwarded to subagent preset creation

--- agental.el
+++ agental.el
@@ -260,8 +260,7 @@ PRESET-ARGS is a plist of additional keyword arguments to pass to
                          preset-args))
 
   (dolist (agent agental-subagents)
-    (agental-make-preset agent
-                         preset-args)))
+    (agental-make-preset agent nil preset-args)))
 
 (provide 'agental)
 ;;; agental.el ends here
